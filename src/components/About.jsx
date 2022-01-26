53182351f14687ab13960514a3a9ea2165879efc
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-01-27 10:26:39 +1300

    updated icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
         <div className="info-container"> 
             <div className="about-wrapper">
                 <h1 className="info-title about-title"> <AnimatedText str={'About Me'}/></h1>
-                <p className="about-tr from-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illo beatae dolorem delectus nobis voluptate fugiat quasi sit repellendus nihil ipsa facere, neque hic incidunt quis velit perferendis nisi itaque, libero rerum eaque. Iste nam cumque, iusto dolorem iure blanditiis suscipit praesentium fugiat reiciendis aut laborum dolore, dolorum deleniti assumenda?</p>
+                <p className="about-tr from-right">I've been passionate about </p>
                 <p className="about-tl from-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis quos voluptate itaque eligendi culpa rerum iure ut totam dicta quasi reprehenderit aliquid, exercitationem placeat saepe quas accusantium sequi dolorem ipsa ea nemo natus soluta suscipit fugiat magni. Obcaecati, ut omnis?</p>
                 <img src={logo} alt="photo" className="my-photo"/>
             </div>      
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ export default function Skills() {
         <div className='info-container'> 
             <div className="skills-wrapper">
                 <h1 className='info-title skills-title'> <AnimatedText str={'Skills'}/></h1>
-                <p className='skills-text'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit explicabo fugit voluptates iusto odio nihil magnam, quis possimus minus, aspernatur excepturi ab sunt molestias totam. I’m proficient with JavaScript(ES6+), React.js, HTML, CSS, Tailwind, RESTful API’s & Github</p>
+                <p className='skills-text'> I've had the opportunities to work on various projects using  I’m proficient with JavaScript(ES6+), React.js, HTML, CSS, Tailwind, RESTful API’s & Github</p>
                 <span className='skills-line'></span>
             </div>
         </div>
