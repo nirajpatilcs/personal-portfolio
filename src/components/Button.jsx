9114c20c09513dcaa47f92e7cb47ae028c61040e
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-01-30 18:16:45 +1300

    updated sbutton styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 
 export default function Button(props) {
     return(
-        // <div className='btn'>
-        //     {props.str} 
-        // </div> 
 
-        <button>
-            <div className="svg-wrapper-1">
-                <div className="svg-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-                </svg>
-                </div>
-            </div>
+        <button class="cta">
             <span>{props.str}</span>
+            <svg width="15px" height="10px" viewBox="0 0 13 10">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
         </button>
     )
 }
